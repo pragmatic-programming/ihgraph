0fdd48facf7ddc5341a08003c4c50e9ac3e6247e
Steven Smyth <ssm@informatik.uni-kiel.de>
2023-11-06 14:31:03 +0100

    Add Annotations to index

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
+export * from './graph/Annotatable';
+export * from './graph/Annotation';
 export * from './graph/EdgeType';
 export * from './graph/IHGraph';
 export * from './graph/IHFactory';
